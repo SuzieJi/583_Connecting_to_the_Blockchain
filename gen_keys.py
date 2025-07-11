72a8df2acf5401908ce41b5a508b5a0374c5294a
Suxin Ji <suxinji@seas.upenn.edu>
2025-07-11 03:35:46 +0000

    add gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -22,12 +22,9 @@ def sign_message(challenge, filename="secret_key.txt"):
     # TODO recover your account information for your private key and sign the given challenge
     # Use the code from the signatures assignment to sign the given challenge
     private_key = key[0].strip()
-    account_object = w3.eth.account.from_key(private_key)
-    eth_addr = account_object.address
-    signed_message = w3.eth.account.sign_message(message, private_key=private_key)
-
-
-
+    acct = eth_account.Account.from_key(private_key)
+    eth_addr = acct.address
+    signed_message = acct.sign_message(message)
 
     assert eth_account.Account.recover_message(message,signature=signed_message.signature.hex()) == eth_addr, f"Failed to sign message properly"
 
